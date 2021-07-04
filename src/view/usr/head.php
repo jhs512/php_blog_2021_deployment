91588b2216b7671cb7d28c2e26ee8756195a1b1a
jhs512 <jangka512@gmail.com>
2021-07-04 17:50:32 +0900

    상단바, 하단바 색상 변경

--- src/view/usr/head.php
+++ src/view/usr/head.php
@@ -52,11 +52,11 @@ $loginedMember = $_REQUEST['App__loginedMember'];
 </head>
 <body>
 <div class="site-wrap min-h-screen flex flex-col pt-10">
-    <header class="top-bar fixed top-0 inset-x-0 bg-black text-white h-10">
+    <header class="top-bar fixed top-0 inset-x-0 text-white h-10">
         <div class="container mx-auto h-full flex">
             <a href="/" class="top-bar__logo px-5 flex items-center">
-                <span><i class="fas fa-lemon"></i></span>
-                <span class="ml-2 font-bold hidden sm:inline">LEMON BLOG</span>
+                <span><i class="fas fa-server"></i></span>
+                <span class="ml-2 font-bold hidden sm:inline">CENT BLOG</span>
             </a>
 
             <div class="flex-grow"></div>
